3d2b7077c7376630b58873b9da548bfb7e61a414
Viennarz Curtiz <viennarzc@gmail.com>
2022-02-07 08:34:03 +0800

    Initial Commit

---
+++ CatsuDogsu/CatsuDogsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsuDogsuApp.swift
+//  CatsuDogsu
+//
+//  Created by Viennarz Curtiz on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CatsuDogsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatsuDogsu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CatsuDogsu
+//
+//  Created by Viennarz Curtiz on 2/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
